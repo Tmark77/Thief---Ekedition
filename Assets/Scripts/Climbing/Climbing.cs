67fd396ea14be89c2765e1ff6ecf18a71081b8fa
Tmark77 <32817053+Tmark77@users.noreply.github.com>
2017-11-12 17:04:07 +0100

    Mászás animációval
    A Character scene-ben a Climbing mappában lévő 2 új script:
    Climbing: animáció segitségével mászik fel az érzékelővel ellátott objektumon.
    LedgeHelp: olyan objektumok alá tesz átmenetileg egy tartó objektumot, amelyeknél leesnénk a levegőből.
    
    A mászáshoz szükséges átmeneti gomb az E betű.

---
+++ Assets/Scripts/Climbing/Climbing.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class Climbing : MonoBehaviour {
+
+	private bool canClimb;
+	private Rigidbody rb;
+	private RigidbodyFirstPersonController cc;
+	public Animator anim;
+	public Camera climbCam;
+	public Camera mainCam;
+
+	public static bool alatetVisible;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		cc = GetComponent<RigidbodyFirstPersonController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		if(canClimb && Input.GetKeyDown(KeyCode.E))
+			{
+			mainCam.depth = 0;
+			climbCam.depth = 1;
+			//cc.enabled = false;
+			rb.useGravity = false;
+			rb.isKinematic = false;
+			anim.SetTrigger ("Climb");
+			StartCoroutine (afterClimb ());
+			}
+	}
+
+	IEnumerator afterClimb() {
+		yield return new WaitForSeconds (2);
+		mainCam.depth = 1;
+		climbCam.depth = 0;
+		//cc.enabled = true;
+		rb.useGravity = true;
+		rb.isKinematic = true;
+		transform.position = climbCam.transform.position;
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Ledge") {
+			canClimb = true;
+			alatetVisible = true;
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		canClimb = false;
+		alatetVisible = false;
+	}
+}
---
+++ Assets/Scripts/Climbing/LedgeHelp.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LedgeHelp : MonoBehaviour {
+
+	private BoxCollider bc;
+
+	// Use this for initialization
+	void Start () {
+		bc = GetComponent<BoxCollider> ();
+		bc.isTrigger = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Climbing.alatetVisible == true) {
+			bc.isTrigger = false;
+		} else {
+			bc.isTrigger = true;
+		}
+	}
+}
