0b294cb234860e78b82a8b4caadf8553aae5f7db
nagymarton <icram96@gmail.com>
2018-01-10 19:44:55 +0100

    menürendszer
    főmenü létrehozva, beállítások menüjét kell még megcsinálni.

---
+++ Assets/Scripts/Menu/MenuScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour {
+    public Canvas mainMenu;
+    public Canvas quitMenu;
+
+	// Use this for initialization
+	void Start () {
+        quitMenu = quitMenu.GetComponent<Canvas>();
+        quitMenu = quitMenu.GetComponent<Canvas>();
+
+        mainMenu.enabled = true;
+        quitMenu.enabled = false;
+    }
+	
+    public void ExitPress()
+    {
+        mainMenu.enabled = false;
+        quitMenu.enabled = true;
+
+    }
+    public void NoPress()
+    {
+        mainMenu.enabled = true;
+        quitMenu.enabled = false;
+    }
+
+    public void StartLevel()
+    {
+        Debug.Log("asd");
+        SceneManager.LoadScene("Character");
+
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit(); 
+    }
+}
