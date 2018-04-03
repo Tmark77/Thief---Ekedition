073236c41e15a79f227875382cd12f33f14f3d5a
nagymarton <icram96@gmail.com>
2018-04-03 13:03:49 +0200

    inventory
    dictionary-s inventory elkezdve

--- Assets/_Scripts/Player/PlayerInventory.cs
+++ Assets/_Scripts/Player/PlayerInventory.cs
@@ -16,8 +16,12 @@ public class PlayerInventory : MonoBehaviour {
 	public Text otherValue;
 
 	//List<int> e = new List<int>();
-	List<Equipment> e = new List<Equipment>();
-	int i;
+	//List<Equipment> e = new List<Equipment>();
+    Dictionary<Equipment, int> equipment = new Dictionary<Equipment, int>();
+	//int i;
+
+    Equipment eKey;
+    int eValue;
 
 
 	// Use this for initialization
@@ -25,7 +29,10 @@ public class PlayerInventory : MonoBehaviour {
 		collectedGemsValue = 0;
 		collectedGoldValue = 0;
 		collectedOtherValue = 0;
-		i = 0;
+		//i = 0;
+
+        eKey = null;
+        eValue = 0;
 		
 	}
 	
@@ -55,35 +62,115 @@ public class PlayerInventory : MonoBehaviour {
 	//public void NewItem(int item)
 	public void NewItem(Equipment item)
 	{
-		e.Add (item);
-		i = e.Count - 1;
-		Debug.Log (e[i]);
-	}
+        eKey = item;
+
+        eValue = 1;
+        if (equipment.ContainsKey(eKey))
+        {
+            
+            eValue = equipment[eKey] + 1;
+            Debug.Log(eValue  + " bennevan ");
+            equipment[eKey] = eValue;
+        }
+        else
+        {
+            equipment.Add(eKey, eValue);
+        }
+        
+
+        Debug.Log(eKey + " " + eValue);
+
+
+
+        //      e.Add (item);
+        //i = e.Count - 1;
+        //Debug.Log (e[i]);
+    }
 
 	public void UseItem()
 	{
-		e[i].Use(hand);
-        //Equipment ep = e[i];
-        e.RemoveAt(i);
-        //Destroy(ep.gameObject);
-        NextItem();
+        eKey.Use(hand);
+        if (eValue <= 1)
+        {
+            equipment.Remove(eKey);
+            NextItem();
+        }
+        else
+        {
+            eValue -= 1;
+            equipment[eKey] = eValue;
+        }
+
+
+		//e[i].Use(hand);
+  //      //Equipment ep = e[i];
+  //      e.RemoveAt(i);
+  //      //Destroy(ep.gameObject);
+  //      NextItem();
     }
 
 	private void NextItem()
 	{
-		i++;
-		if (i >= e.Count)
-			i = 0;
-		Debug.Log (e [i]);
-	}
+        if (equipment.Count > 1)
+        {
+            Equipment k = eKey;
+            int v = eValue;
+            bool found = true;
+            foreach (var e in equipment)
+            {
+                if (found)
+                {
+                    k = e.Key;
+                    v = e.Value;
+                    found = false;
+                }
+                if (e.Key == eKey)
+                {
+                    found = true;
+                }
+                //Debug.Log("{0}, {1}", pair.Key, pair.Value);
+            }
+            eKey = k;
+            eValue = v;
+        }
+        Debug.Log(eKey + " " + eValue);
+
+        //i++;
+        //if (i >= e.Count)
+        //	i = 0;
+        //Debug.Log (e [i]);
+    }
 
 	private void PrevItem()
 	{
-		i--;
-		if (i < 0)
-			i = e.Count-1;
-		Debug.Log (e [i]);
-	}
+        int count = equipment.Count;
+        if (count > 1)
+        {
+            Equipment k = eKey;
+            int v = eValue;
+            bool found = false;
+            foreach (var e in equipment)
+            {
+                if (e.Key == eKey)
+                {
+                    found = true;
+                }
+                if (!found)
+                {
+                    k = e.Key;
+                    v = e.Value;
+                }
+                //Debug.Log("{0}, {1}", pair.Key, pair.Value);
+            }
+            eKey = k;
+            eValue = v;
+        }
+        Debug.Log(eKey + " " + eValue);
+        //i--;
+        //if (i < 0)
+        //	i = e.Count-1;
+        //Debug.Log (e [i]);
+    }
 
 	public void AddGem (int value)
 	{
