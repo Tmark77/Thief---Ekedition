56b286bfdd13c6e5be98f23334a8d2a70b705fe1
Tmark77 <32817053+Tmark77@users.noreply.github.com>
2018-04-07 18:33:34 +0200

    Lockpick fix
    Hibátlanul működik a lockpickes gömböcke mozgás, a playable levelen kipróbálható

--- Assets/LockPickEvent.cs
+++ Assets/LockPickEvent.cs
@@ -3,17 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LockPickEvent : MonoBehaviour {
-    
+
+    public GameObject start;
+    public GameObject end;
+    bool irany;
 
     // Use this for initialization
-    void Awake ()
+    void Start ()
     {
         
     }
 	
 	// Update is called once per frame
 	void Update ()
-    { 
-        this.transform.Translate(Mathf.Sin(Time.time * 3) / 120f, 0f, 0f, Space.Self);
+    {
+        if (this.transform.position == end.transform.position)
+        {
+            irany = false;
+        }
+        else if (this.transform.position == start.transform.position)
+        {
+            irany = true;
+        }
+
+        if (irany)
+        {
+            this.transform.position = Vector3.MoveTowards(this.transform.position, end.transform.position, Time.deltaTime / 5f);
+        }
+        else
+        {
+            this.transform.position = Vector3.MoveTowards(this.transform.position, start.transform.position, Time.deltaTime / 5f);
+        }
     }
+
+
 }
